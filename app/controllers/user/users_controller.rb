367e55e227a466d918cc06b13e04c796a2045974
EC2 Default User <ec2-user@ip-172-31-0-131.ap-northeast-1.compute.internal>
2023-03-20 09:13:13 +0000

    マイページ修正

--- app/controllers/user/users_controller.rb
+++ app/controllers/user/users_controller.rb
@@ -1,18 +1,30 @@
 class User::UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @posts = @user.posts
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def quit
+  end
+
+  def update
+    user = User.find(params[:id])
+    if user.update(user_params)
+      flash[:notice]="You have updated user successfully."
+      redirect_to user_user_path(current_user)
+    else
+      render:edit
+    end
   end
 
 
   private
 
   def user_params
-      params.require(:user).permit(:screen_name, :profile_image_url, :sex)
+      params.require(:user).permit(:screen_name, :profile_image_url)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,15 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :store_comments, dependent: :destroy
+  has_one_attached :profile_image
+
+  def get_profile_image(width, height)
+    unless profile_image.attached?
+      profile_image.attach(io: File.open(file_path), filename: 'no_image.jpeg', content_type: 'image/jpeg')
+    end
+    profile_image.variant(resize_to_limit: [width, height]).processed
+  end
+
 
   def active_for_authentication?
     super && (is_deleted == false)
